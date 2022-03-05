cfaf7b0f145e524bbc74687b8c4a56f334c76c0a
Suresh James <suresh.james@wolterskluwer.com>
2022-03-04 21:04:16 -0500

    v0.02

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -13,6 +14,13 @@ namespace dotnetmicroservice
 {
     public class Startup
     {
+        private readonly IDictionary<Guid, Vehicle> _vehicles;
+
+        public Startup()
+        {
+            _vehicles = new Dictionary<Guid, Vehicle>(8);
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -30,21 +38,55 @@ namespace dotnetmicroservice
                 /// ... GET
                 endpoints.MapGet("/vehicles", async context =>
                 {
-                    var vehicles = new Vehicle[10];
-
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsJsonAsync(_vehicles.Values);
                 });
 
                 /// ... POST
-                endpoints.MapPost("/vehicles", async context => 
+                endpoints.MapPost("/vehicles", async context =>
                 {
-                    context.Response.StatusCode = (int)HttpStatusCode.Created;
-                    await context.Response.WriteAsync("Got it!");
+                    JsonSerializerOptions options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true, };
+                    try
+                    {
+                        var vehicle = context.Request.ReadFromJsonAsync<Vehicle>(options).Result;
+                        _vehicles.Add(vehicle.Id, vehicle);
+                        context.Response.StatusCode = (int)HttpStatusCode.Created;
+                    }
+                    catch (Exception ex)
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    }
                 });
 
+                /// ... PUT
                 endpoints.MapPut("/vehicles/{id}", async context => 
                 {
-                    await context.Response.WriteAsync("");
+                    JsonSerializerOptions options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true, };
+                    var vehicle = context.Request.ReadFromJsonAsync<Vehicle>(options).Result;
+
+                    Guid id = new Guid((string)context.Request.RouteValues["id"]);
+                    if (_vehicles.ContainsKey(id))
+                    {
+                        _vehicles[id] = vehicle;
+                    }
+                    else
+                    {
+                        _vehicles.Add(vehicle.Id, vehicle);
+                    }
+                });
+
+                endpoints.MapDelete("/vehicles/{id}", async context =>
+                {
+                    Guid id = new Guid((string)context.Request.RouteValues["id"]);
+
+                    if (_vehicles.ContainsKey(id))
+                    {
+                        _vehicles.Remove(id);
+                        context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.NoContent;
+                    }
                 });
             });
         }
--- Vehicle.cs
+++ Vehicle.cs
@@ -7,13 +7,14 @@ namespace dotnetmicroservice
 {
     public class Vehicle
     {
-        public EnumTypes Type { get; set; }
+        public Guid Id { get; set; }
+        public EnumTypes? Type { get; set; }
         public string Description { get; set; }
-        public int Length { get; set; }
-        public int Width { get; set; }
-        public decimal Cost { get; set; }
-        public DateTime ManufacturedOn { get; set; }
-        public long Mileage { get; set; }
+        public int? Length { get; set; }
+        public int? Width { get; set; }
+        public decimal? Cost { get; set; }
+        public DateTime? ManufacturedOn { get; set; }
+        public long? Mileage { get; set; }
 
         public enum EnumTypes
         {
